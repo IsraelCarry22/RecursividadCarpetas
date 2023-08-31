587a6cf6831b9a5f57060cd2ea14cdde3960bf16
Israe <Israe@ISRAEL>
2023-08-31 15:04:05 -0600

    Agregar archivos de proyecto.

---
+++ RecursividadCarpetas/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace RecursividadCarpetas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escribe la ruta de la carpeta: ");
+            string RutaCarpeta = Console.ReadLine();
+            if (!Directory.Exists(RutaCarpeta))
+            {
+                Console.WriteLine("No existe la ruta :(");
+                Console.WriteLine("Pulse cualquier tecla para salir...");
+            }
+            ListaRecursividadCarpetas(RutaCarpeta);
+            Console.WriteLine("Pulse cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+
+        public static void ListaRecursividadCarpetas(string RutaCarpeta)
+        {
+            DirectoryInfo directory = new DirectoryInfo(RutaCarpeta);
+            foreach (FileInfo Archivo in directory.GetFiles())
+            {
+                string nombre = Archivo.Name;
+                string extension = Archivo.Extension;
+                Console.WriteLine($"Nombre: {nombre} || extension: {extension}.");
+            }
+            foreach (DirectoryInfo subdirectory in directory.GetDirectories())
+            {
+                ListaRecursividadCarpetas(subdirectory.FullName);
+            }
+        }
+    }
+}
